a748ccdef9bced5d6d18473637f47273396ee9dc
queensburybill <queensburybill@gmail.com>
2018-09-12 08:28:20 -0400

    modified and passed palindrome.js

--- exercises/01-palindrome/palindrome.js
+++ exercises/01-palindrome/palindrome.js
@@ -4,7 +4,15 @@
  * @param  {[string]}  string
  * @return {Boolean}
  */
-function isPalindrome(str) {}
+function isPalindrome(str) {
+  console.log(str);
+  let l = str.length - 1;
+  for (let i = 0; i < Math.floor(str.length/2); i++) {
+      if (str[i] !== str[l]) return false;
+      l--;
+  }
+  return true;
+}
 
 module.exports = {
   isPalindrome
