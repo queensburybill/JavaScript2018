6c80ad7f8a910c44c828414837743e3e302ecd53
William Peirce <bill@seeyourcampsite.com>
2018-09-25 10:41:42 -0400

    Finished unit testing exercises

--- test/05-unit-testing/unit-testing-test.js
+++ test/05-unit-testing/unit-testing-test.js
@@ -15,7 +15,7 @@ const { expect } = require("chai");
 const {
   add,
   subtract,
-  // Include multiply here
+  multiply,
   findAdults
 } = require("../../exercises/05-unit-testing/unit-testing");
 
@@ -28,30 +28,34 @@ describe("unit testing exercise", function() {
   });
 
   describe("subtract", function() {
-    // Write a test for subtract here
+    it("should subtract two numbers", function() {
+      var diff = subtract(6, 4);
+      expect(diff).to.equal(2);
+    })
   });
 
   describe("multiply", function() {
-    // Write the function multiply in `exercises/05-unit-testing/unit-testing-code.js`
-    // Export / import the multiply function
-    // Write a test for multiply here
+    it("should multiply two numbers", function() {
+      var product = multiply(3, 5);
+      expect(product).to.equal(15);
+    })
   });
 
   describe("findAdults", function() {
-    // Remove the `.skip` when you are ready to write this test
-    it.skip("will find, in a multidimensional array, all the people older than 18", function() {
-      // Complete the unit test for findAdults here
-      // Hint: Arrays are passed by reference, so you will need to call on a test that deeply compares values
-      // @see https://www.chaijs.com/api/bdd/
+    it("will find, in a multidimensional array, all the people older than 18", function() {
       var people = [
         { name: "Janet", age: 43 },
         { name: "Aiden", age: 10 },
         { name: "Chloe", age: 16 }
       ];
+      var adults = [
+        { name: "Janet", age: 43 }
+      ]
+      var result = findAdults(people);
+      expect(result).to.deep.equal(adults);
     });
 
-    // Remove the `.skip` when you are ready to write this test
-    it.skip("will return an empty array if no adults are found", function() {
+    it("will return an empty array if no adults are found", function() {
       // Complete the unit test for findAdults here, where you use a different assertion than `.equal()`
       // @see https://www.chaijs.com/api/bdd/
       var people = [
@@ -59,6 +63,8 @@ describe("unit testing exercise", function() {
         { name: "Emma", age: 17 },
         { name: "Ethan", age: 8 }
       ];
+      var result = findAdults(people);
+      expect(result).to.be.empty;
     });
   });
 });
